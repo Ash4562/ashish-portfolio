f87dffde46742bf74203c2eeb5ca06f584dec3e1
Your Name <you@example.com>
2024-02-17 13:16:40 +0530

    third

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require("express")
 const cors = require("cors")
 require("dotenv").config({ path: "./.env" })
-const app = express()
 const path = require("path")
-require("dotenv").config({ path: "./.env" })
 
 
+
+const app = express()
 app.use(express.json())
-// app.use(path.join(__dirname, "dist"))
+app.use(path.join(__dirname, "dist"))
 app.use(cors({
     origin: "http://localhost:5173"
 }))
 app.use("/api/user", require("./router/emailRouters"))
 
 
-// app.use("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "dist", "index.html"))
-// })
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+})
 
 
-app.listen(5000, console.log("server running"))
+app.listen(process.env.PORT, console.log("server running"))
